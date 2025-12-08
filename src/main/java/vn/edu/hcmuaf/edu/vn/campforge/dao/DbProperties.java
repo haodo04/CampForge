230ab2055bb06cccfd9003b0748980f4e12382ed
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-12-08 19:31:14 +0700

    feat: create dbconnect class to connect mysql

--- src/main/java/vn/edu/hcmuaf/edu/vn/campforge/dao/DbProperties.java
+++ src/main/java/vn/edu/hcmuaf/edu/vn/campforge/dao/DbProperties.java
@@ -1,2 +1,58 @@
-package vn.edu.hcmuaf.edu.vn.campforge.dao;public class DbProperties {
+package vn.edu.hcmuaf.edu.vn.campforge.dao;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DbProperties {
+    private static final Properties prod = new Properties();
+
+    static {
+        try (InputStream input = DbProperties.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                throw new RuntimeException("db.properties not found in classpath");
+            }
+            prod.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load db.properties", e);
+        }
+    }
+
+    public static String host() {
+        return get("db.host");
+    }
+
+    public static int port() {
+        try {
+            return Integer.parseInt(get("db.port"));
+        } catch (NumberFormatException e) {
+            return 3306;
+        }
+    }
+
+    public static String username() {
+        return get("db.user");
+    }
+
+    public static String password() {
+        return get("db.password");
+    }
+
+    public static String dbname() {
+        return get("db.dbname");
+    }
+
+    public static String option() {
+        return get("db.option");
+    }
+
+    private static String get(String key) {
+        String value = prod.getProperty(key);
+        if (value == null) {
+            throw new RuntimeException("Missing key in db.properties: " + key);
+        }
+        return value;
+    }
 }
+
