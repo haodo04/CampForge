5e976bba1347a0d09bd2ec798d120c2b4a03797c
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-12-14 17:21:44 +0700

    refactor: update get data from db, service class

--- src/main/java/vn/edu/hcmuaf/edu/vn/campforge/dao/ProductDAO.java
+++ src/main/java/vn/edu/hcmuaf/edu/vn/campforge/dao/ProductDAO.java
@@ -4,174 +4,134 @@ import vn.edu.hcmuaf.edu.vn.campforge.dao.db.DbConnect;
 import vn.edu.hcmuaf.edu.vn.campforge.model.Product;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ProductDAO {
 
-    public static List<Product> getProductsByCategory(int categoryId) {
+    private static final String BASE_SELECT = """
+        SELECT
+            p.id,
+            p.cateId,
+            p.brandId,
+            p.proName,
+            p.price,
+            p.description,
+            p.sold,
+            p.createAt,
+            p.isDelete,
+            i.path AS image,
+            b.name AS brandName,
+            c.cateName AS cateName
+        FROM products p
+        LEFT JOIN product_imgs i
+            ON p.id = i.product_id AND i.position = 1
+        LEFT JOIN brand b
+            ON p.brandId = b.id
+        LEFT JOIN categories c
+            ON p.cateId = c.id
+    """;
+
+    private static Product mapRow(ResultSet rs) throws SQLException {
+        Product p = new Product();
+        p.setId(rs.getInt("id"));
+        p.setCateId(rs.getInt("cateId"));
+        p.setBrandId(rs.getInt("brandId"));
+        p.setProName(rs.getString("proName"));
+        p.setPrice(rs.getDouble("price"));
+        p.setDescription(rs.getString("description"));
+        p.setSold(rs.getInt("sold"));
+        p.setCreateAt(rs.getTimestamp("createAt"));
+        p.setIsDelete(rs.getInt("isDelete"));
+        p.setImage(rs.getString("image"));
+        p.setBrandName(rs.getString("brandName"));
+        p.setCateName(rs.getString("cateName"));
+        return p;
+    }
+
+    public static List<Product> findProducts(Integer cateId, Integer brandId,
+                                             Double min, Double max,
+                                             int limit, int offset) {
         List<Product> list = new ArrayList<>();
 
-        String sql = """
-            SELECT p.id, p.proName, p.price, p.description, p.cateId, p.brandId,
-                   i.path AS image,
-                    b.name AS brandName
+        StringBuilder sql = new StringBuilder(BASE_SELECT);
+        sql.append(" WHERE p.isDelete = 0 ");
+
+        List<Object> params = new ArrayList<>();
+
+        if (cateId != null) {
+            sql.append(" AND p.cateId = ? ");
+            params.add(cateId);
+        }
+        if (brandId != null) {
+            sql.append(" AND p.brandId = ? ");
+            params.add(brandId);
+        }
+        if (min != null && max != null) {
+            sql.append(" AND p.price BETWEEN ? AND ? ");
+            params.add(min);
+            params.add(max);
+        }
+
+        sql.append(" ORDER BY p.createAt DESC ");
+        sql.append(" LIMIT ? OFFSET ? ");
+        params.add(limit);
+        params.add(offset);
+
+        try (Connection conn = DbConnect.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql.toString())) {
+
+            for (int i = 0; i < params.size(); i++) {
+                ps.setObject(i + 1, params.get(i));
+            }
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) list.add(mapRow(rs));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static int countProducts(Integer cateId, Integer brandId, Double min, Double max) {
+        StringBuilder sql = new StringBuilder("""
+            SELECT COUNT(*)
             FROM products p
-            LEFT JOIN product_imgs i 
-                   ON p.id = i.product_id AND i.position = 1
-            LEFT JOIN brand b
-                    ON p.brandId = b.id
-            WHERE p.cateId = ?
-        """;
+            WHERE p.isDelete = 0
+        """);
+
+        List<Object> params = new ArrayList<>();
+
+        if (cateId != null) {
+            sql.append(" AND p.cateId = ? ");
+            params.add(cateId);
+        }
+        if (brandId != null) {
+            sql.append(" AND p.brandId = ? ");
+            params.add(brandId);
+        }
+        if (min != null && max != null) {
+            sql.append(" AND p.price BETWEEN ? AND ? ");
+            params.add(min);
+            params.add(max);
+        }
 
         try (Connection conn = DbConnect.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+             PreparedStatement ps = conn.prepareStatement(sql.toString())) {
 
-            ps.setInt(1, categoryId);
-            ResultSet rs = ps.executeQuery();
+            for (int i = 0; i < params.size(); i++) {
+                ps.setObject(i + 1, params.get(i));
+            }
 
-            while (rs.next()) {
-                Product p = new Product(
-                        rs.getInt("id"),
-                        rs.getString("proName"),
-                        rs.getDouble("price"),
-                        rs.getString("description"),
-                        rs.getInt("cateId"),
-                        rs.getInt("brandId"),
-                        rs.getString("brandName"),
-                        rs.getString("image")
-                );
-                list.add(p);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) return rs.getInt(1);
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return list;
+        return 0;
     }
-
-    public static List<Product> getAllProducts() {
-        List<Product> list = new ArrayList<>();
-
-        String sql = """
-        SELECT p.id, p.proName, p.price, p.description, p.cateId, p.brandId,
-               i.path AS image,
-                b.name AS brandName
-        FROM products p
-        LEFT JOIN product_imgs i 
-               ON p.id = i.product_id AND i.position = 1
-        LEFT JOIN brand b
-                ON p.brandId = b.id
-    """;
-
-        try (Connection conn = DbConnect.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                Product p = new Product(
-                        rs.getInt("id"),
-                        rs.getString("proName"),
-                        rs.getDouble("price"),
-                        rs.getString("description"),
-                        rs.getInt("cateId"),
-                        rs.getInt("brandId"),
-                        rs.getString("brandName"),
-                        rs.getString("image")
-                );
-                list.add(p);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return list;
-    }
-    public static List<Product> getProductsByBrand(int brandId) {
-        List<Product> list = new ArrayList<>();
-
-        String sql = """
-        SELECT p.id, p.proName, p.price, p.description, p.cateId, p.brandId,
-               i.path AS image,
-                b.name AS brandName
-        FROM products p
-        LEFT JOIN product_imgs i 
-               ON p.id = i.product_id AND i.position = 1
-        LEFT JOIN brand b
-                ON p.brandId = b.id
-        WHERE p.brandId = ?
-        
-    """;
-
-        try (Connection conn = DbConnect.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ps.setInt(1, brandId);
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                Product p = new Product(
-                        rs.getInt("id"),
-                        rs.getString("proName"),
-                        rs.getDouble("price"),
-                        rs.getString("description"),
-                        rs.getInt("cateId"),
-                        rs.getInt("brandId"),
-                        rs.getString("brandName"),
-                        rs.getString("image")
-                );
-                list.add(p);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    public static List<Product> getProductsByPrice(double min, double max) {
-        List<Product> list = new ArrayList<>();
-
-        String sql = """
-        SELECT p.id, p.proName, p.price, p.description, p.cateId, p.brandId,
-               i.path AS image,
-                b.name AS brandName
-        FROM products p
-        LEFT JOIN product_imgs i 
-               ON p.id = i.product_id AND i.position = 1
-        LEFT JOIN brand b
-               ON p.brandId = b.id
-        WHERE p.price BETWEEN ? AND ?
-    """;
-
-        try (Connection conn = DbConnect.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ps.setDouble(1, min);
-            ps.setDouble(2, max);
-
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                Product p = new Product(
-                        rs.getInt("id"),
-                        rs.getString("proName"),
-                        rs.getDouble("price"),
-                        rs.getString("description"),
-                        rs.getInt("cateId"),
-                        rs.getInt("brandId"),
-                        rs.getString("brandName"),
-                        rs.getString("image")
-                );
-                list.add(p);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
 }
