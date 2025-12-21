e03513f56dbfb83d5f8e88877cc8078fb45c7680
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-12-21 15:10:18 +0700

    refactor: change dbconnect class using jdbi connection

--- src/main/java/vn/edu/hcmuaf/edu/vn/campforge/dao/db/DbConnect.java
+++ src/main/java/vn/edu/hcmuaf/edu/vn/campforge/dao/db/DbConnect.java
@@ -1,35 +1,53 @@
 package vn.edu.hcmuaf.edu.vn.campforge.dao.db;
 
-import java.sql.*;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.jdbi.v3.core.Jdbi;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DbConnect {
-    static String url = "jdbc:mysql://" + DbProperties.host() + ":" + DbProperties.port() + "/" +
-            DbProperties.dbname() + "?" + DbProperties.option();
 
-    static Connection conn;
-    private static final ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
-    private static volatile boolean printed = false;
+    private static final HikariDataSource DATA_SOURCE;
+    private static final Jdbi JDBI;
 
-    public static Connection getConnection() {
-        try {
-            if (conn == null || conn.isClosed()) {
-                conn = makeConnect();
-                threadLocal.set(conn);
+    static {
+        HikariConfig cfg = new HikariConfig();
 
-                if (!printed) {
-                    printed = true;
-                    System.out.println("DB connected.");
-                }
-            }
-            return conn;
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
+        cfg.setJdbcUrl(
+                "jdbc:mysql://" +
+                        DbProperties.host() + ":" +
+                        DbProperties.port() + "/" +
+                        DbProperties.dbname() + "?" +
+                        DbProperties.option()
+        );
+
+        cfg.setUsername(DbProperties.username());
+        cfg.setPassword(DbProperties.password());
+
+        // pool config
+        cfg.setMaximumPoolSize(10);
+        cfg.setMinimumIdle(2);
+        cfg.setConnectionTimeout(30000);
+        cfg.setIdleTimeout(300000);
+        cfg.setMaxLifetime(1800000);
+
+        // cfg.setLeakDetectionThreshold(20000);
+
+        DATA_SOURCE = new HikariDataSource(cfg);
+
+        // Tạo JDBI dùng chung pool
+        JDBI = Jdbi.create(DATA_SOURCE);
+
+        System.out.println("HikariCP pool initialized.");
     }
 
-    private static Connection makeConnect() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(url, DbProperties.username(), DbProperties.password());
+    public static Connection getConnection() throws SQLException {
+        return DATA_SOURCE.getConnection();
+    }
+
+    public static Jdbi getJdbi() {
+        return JDBI;
     }
 }
